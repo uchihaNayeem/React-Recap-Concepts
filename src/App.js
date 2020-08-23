06a8eb88cb901dfde1da7bd3164039dea0d6a34f
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-08-23 20:50:12 +0600

    useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <MovieCounter></MovieCounter>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
 }
 
+
+function MovieCounter(){
+
+  const [count, setCount] = useState(0)
+
+  const handleCount = () => setCount(count +1)
+  
+  return(
+    <div>
+      <button onClick={handleCount}>Add Movie</button>
+      <h1>Increase: {count} </h1>
+      <MovieCount countMovie={count}></MovieCount>
+      <MovieCount countMovie={count +1}></MovieCount>
+      <MovieCount countMovie={count +20}></MovieCount>
+    </div>
+    
+  )
+}
+
 export default App;
+
+
+function MovieCount(props){
+
+  return(
+    <div>
+      <h3>Movie Counter:{props.countMovie} </h3>
+    </div>
+  )
+}
